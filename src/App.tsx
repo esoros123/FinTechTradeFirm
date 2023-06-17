09eea393a9e1b14d14310eff820ac1f70e60764e
Eric Soros <ericsoros@Erics-MacBook-Pro-2.local>
2023-06-17 10:39:01 -0400

    adding default values

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [amount, setAmount] = useState("")
-  const [interest, setInterest] = useState("")
-  const [tradeDays, setTradeDays] = useState("")
+  const [amount, setAmount] = useState("100")
+  const [interest, setInterest] = useState("1.00")
+  const [tradeDays, setTradeDays] = useState("100")
   const [err, setErr] = useState("")
   const [result, setResult] = useState<Array<number>>([])
 
@@ -53,13 +53,13 @@ function App() {
         <h3 style={{placeSelf: "center", fontWeight: "lighter", textDecoration: "underline"}}>Fin Tech Trade Firm</h3>
         <div className='form'>          
           <label htmlFor="fname">Starting Amount:</label><br />
-          <input onChange={(e) => setAmount(e.currentTarget.value)} style={{marginBottom: ".25rem"}} type="text" id="fname" name="fname" /><br />
+          <input value={amount} onChange={(e) => setAmount(e.currentTarget.value)} style={{marginBottom: ".25rem"}} type="text" id="fname" name="fname" /><br />
         
           <label htmlFor="lname">Interest (Percent:)</label><br />
-          <input onChange={(e) => setInterest(e.currentTarget.value)}  style={{marginBottom: ".25rem"}} type="text" id="lname" name="lname" /><br />
+          <input value={interest} onChange={(e) => setInterest(e.currentTarget.value)}  style={{marginBottom: ".25rem"}} type="text" id="lname" name="lname" /><br />
       
           <label htmlFor="days">Trade Days</label><br />
-          <input onChange={(e) => setTradeDays(e.currentTarget.value)}  style={{marginBottom: ".50rem"}} type="text" id="days" name="days" /><br />
+          <input value={tradeDays} onChange={(e) => setTradeDays(e.currentTarget.value)}  style={{marginBottom: ".50rem"}} type="text" id="days" name="days" /><br />
         
           <button onClick={calculateAmount} value="Submit" style={{width: inputWidth, height: inputHeight! * 1.1, backgroundColor: "azure", border: "solid azure .1rem", borderRadius: ".10rem"}} >Submit</button>
           {
