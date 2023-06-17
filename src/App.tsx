481928652bbededecbc79afbcbcf321dfe7cabfd
Eric Soros <ericsoros@Erics-MacBook-Pro-2.local>
2023-06-17 14:38:41 -0400

    adding font

--- src/App.tsx
+++ src/App.tsx
@@ -29,8 +29,8 @@ function App() {
       return
     }
 
-    let interst_float = parseFloat(interest)
-    if(!interst_float) {
+    let interest_float = parseFloat(interest)
+    if(!interest_float) {
       setErr("unable to parse interest")
       return
     }
@@ -41,33 +41,33 @@ function App() {
       return
     }
 
-    interst_float = interst_float * 0.01
+    interest_float = interest_float * 0.01
     let result = [amount_num]
     for(let i = 0; i < tradeDays_num; ++i) {
-      result.push(result[result.length - 1] + (result[result.length - 1] * interst_float))
+      result.push(result[result.length - 1] + (result[result.length - 1] * interest_float))
     }
     setResult(result)
   }
   
   return <div style={{display: "grid", height: "100%", width: "100%", placeContent: "center"}}>
-        <h3 style={{placeSelf: "center", fontWeight: "lighter", textDecoration: "underline"}}>Fin Tech Trade Firm</h3>
+        <h3 style={{placeSelf: "center", fontWeight: "lighter"}}>Compound Interest Calculator</h3>
         <div className='form'>          
-          <label htmlFor="fname">Starting Amount:</label><br />
+          <label htmlFor="fname">Starting Amount</label><br />
           <input value={amount} onChange={(e) => setAmount(e.currentTarget.value)} style={{marginBottom: ".25rem"}} type="text" id="fname" name="fname" /><br />
         
-          <label htmlFor="lname">Interest (Percent:)</label><br />
+          <label htmlFor="lname">Interest (Percent)</label><br />
           <input value={interest} onChange={(e) => setInterest(e.currentTarget.value)}  style={{marginBottom: ".25rem"}} type="text" id="lname" name="lname" /><br />
       
           <label htmlFor="days">Trade Days</label><br />
           <input value={tradeDays} onChange={(e) => setTradeDays(e.currentTarget.value)}  style={{marginBottom: ".50rem"}} type="text" id="days" name="days" /><br />
         
-          <button onClick={calculateAmount} value="Submit" style={{width: inputWidth, height: inputHeight! * 1.1, backgroundColor: "azure", border: "solid azure .1rem", borderRadius: ".10rem"}} >Submit</button>
+          <button onClick={calculateAmount} value="Submit" style={{width: inputWidth, height: inputHeight! * 1.3, backgroundColor: "azure", border: "solid azure .1rem", borderRadius: ".10rem", padding: ".5rem", display: "flex", flexDirection: "column", placeContent: "center", fontSize: ".75rem"}}>Submit</button>
           {
             err ? <p>{err}</p> : <></>
           }
           {
             result.length != 0 && !err ? 
-            <div style={{display: "flex", flexDirection: "column", overflowY: "scroll", height: "40vh"}}>
+            <div className='box' style={{display: "flex", flexDirection: "column", overflowY: "scroll", height: "40vh"}}>
               {
                 result.map((p, i) => <p style={{margin: 0}}>{i + ":   " + p.toString()}</p>) 
               }
